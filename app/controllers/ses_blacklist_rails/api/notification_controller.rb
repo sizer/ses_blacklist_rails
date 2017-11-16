32507789d9272a7b9e6393ad4bd3eb63b123118b
mrdShinse <shinse1128@hotmail.com>
2017-11-16 17:41:36 +0900

    add controller Api::NotificationController `bundle exec rails g controller Api::Notification --no-helper --no-assets`

---
+++ app/controllers/ses_blacklist_rails/api/notification_controller.rb
@@ -0,0 +1,6 @@
+require_dependency "ses_blacklist_rails/application_controller"
+
+module SesBlacklistRails
+  class Api::NotificationController < ApplicationController
+  end
+end
---
+++ spec/controllers/ses_blacklist_rails/api/notification_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module SesBlacklistRails
+  RSpec.describe Api::NotificationController, type: :controller do
+
+  end
+end
