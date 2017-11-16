d2c2862915180491ff3ca90144b83ab5e0d16317
mrdShinse <shinse1128@hotmail.com>
2017-11-16 16:36:59 +0900

    some changes for rspec.

--- lib/ses_blacklist_rails/engine.rb
+++ lib/ses_blacklist_rails/engine.rb
@@ -1,5 +1,9 @@
 module SesBlacklistRails
-  class Engine < ::Rails::Engine
+  class Engine < ::Rails::Engine # :nodoc:
     isolate_namespace SesBlacklistRails
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+    end
   end
 end
